3d6c78b6476ac38b1472f84c33ded30f748e8575
MaiZinMar94 <78303936+MaiZinMar94@users.noreply.github.com>
2021-03-04 18:37:43 +0100

    Update brexit_survey_assessment.R
    Hello, your solutions helped me during the course, therefore I would like to help also others by adding the solutions for the 4th chapter. Do you have the solutions for the rest of the assignment? I'm working my way through them and can add also a bit more on a later stage :)

--- 4-Statistics&Inference/brexit_survey_assessment.R
+++ 4-Statistics&Inference/brexit_survey_assessment.R
@@ -8,6 +8,7 @@ data(brexit_polls)
 
 p <- 0.481    # official proportion voting "Remain"
 d <- 2*p-1    # official spread
+N <- 1500
 
 ########### QUESTION 1 ############
 # The final proportion of voters chossing to "Remain" was p=0.481. Consider a poll
@@ -17,6 +18,18 @@ p*1500
 
 # What is the standard error of the total number of voters in the sample choosing
 #  "Remain"?
-sqrt(p*(1-p)/1500)
+sqrt(N*p*(1-p))
 
 # What is the expected value of X, the proportion of "Remain" voters?
+𝑝=0.481
+
+#What is the standard error of  𝑋̂ , the proportion of "Remain" voters?
+sqrt(p*(1-p)/N)
+
+#What is the expected value of  𝑑 , the spread between the proportion 
+#of "Remain" voters and "Leave" voters?
+ 2*p-1
+
+#What is the standard error of  𝑑 , the spread between the proportion 
+#of "Remain" voters and "Leave" voters?
+2*sqrt(p*(1-p)/N)
